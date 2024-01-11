3c64dc85657de3354e39b6b0b513e124a241b81e
NoDream <1802024110@qq.com>
2024-01-11 02:10:11 +0000

    1

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,11 +17,17 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-sudo apt-get update 
-sudo apt-get install libfuse-dev
 
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-git pull
-./scripts/feeds update -a
-./scripts/feeds install -a
+
+
+
+
+
+# sudo apt-get update 
+# sudo apt-get install libfuse-dev
+
+# sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+# git pull
+# ./scripts/feeds update -a
+# ./scripts/feeds install -a
